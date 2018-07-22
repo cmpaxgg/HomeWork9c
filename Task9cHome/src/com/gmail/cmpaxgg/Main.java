e7bd995356b9654adf5b16d3b74ce8b3dc79c6a3
cmpaxgg <cmpaxgg@gmail.com>
2018-07-22 12:55:13 +0300

    Commit 2,Name of Method change

--- Task9cHome/src/com/gmail/cmpaxgg/Main.java
+++ Task9cHome/src/com/gmail/cmpaxgg/Main.java
@@ -12,13 +12,13 @@ public class Main {
 		// TODO Auto-generated method stub
 		File file = new File("Documentation.txt");
 		try {
-			NumberofReplaySymbol(file);
+			numberofReplaySymbol(file);
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
 	}
 
-	public static void NumberofReplaySymbol(File file) throws IOException{
+	public static void numberofReplaySymbol(File file) throws IOException{
 		Files.lines(Paths.get(file.getAbsolutePath()))
 		.map(n -> n.toUpperCase())
 		.flatMapToInt(n -> n.chars())
